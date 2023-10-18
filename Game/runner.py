24bf12f667be38905bccbd295eab8bf68b8a5479
asutton24 <133972971+asutton24@users.noreply.github.com>
2023-10-18 15:24:03 -0400

    Add files via upload

--- Game/runner.py
+++ Game/runner.py
@@ -1,5 +1,6 @@
 import pygame
 from sprite import *
+from gameObject import *
 
 def main():
     running = True
@@ -8,12 +9,20 @@ def main():
     clock = pygame.time.Clock()
     player = Sprite('player.txt', 100, 100, (255, 255, 255), 30, 10, screen)
     hello = Text('i can now/display/sprites', 0, 0, (255, 255, 255), 5, screen)
+    colorSpr = ColorSprite('colorTest.txt', 100, 100, [(255, 0, 0), (0, 255, 0), (0, 0, 255)], -1, 5, screen)
+    gameObj = GameObject(player, False, 100, False)
+    vel = 5
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((0,0,0))
-        hello.update()
+        colorSpr.update()
+        if colorSpr.getPos()[0] >= 600:
+            vel = -5
+        elif colorSpr.getPos()[0] <= 0:
+            vel = 5
+        colorSpr.move(vel, 0)
         pygame.display.update()
         clock.tick(60)
 
