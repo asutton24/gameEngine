349efb8ac47f2d50a24104ad3a0ca543e62224e3
asutton24 <133972971+asutton24@users.noreply.github.com>
2023-11-16 17:23:35 -0500

    Add files via upload

--- Game/runner.py
+++ Game/runner.py
@@ -12,9 +12,11 @@ def main():
     hello = Text('i can now/display/sprites', 0, 0, (255, 255, 255), 5, screen)
     colorSpr = ColorSprite('colorTest.txt', 100, 100, [(255, 0, 0), (0, 255, 0), (0, 0, 255)], -1, 5, screen)
     p1 = Player(500, 250, screen)
-    block = GameObject(Sprite('block.txt', 400, 200, (255, 255, 255), -1,5, screen), True, 1)
+    block = GameObject(Sprite('block.txt', 400, 200, (255, 255, 255), -1,5, screen), True, 1, False)
     room1 = Room('Manifest\\RoomManifest.txt', 0, (0, 0, 0), screen)
     room1.setDoors(True, True, True, True)
+    testEnemy = Enemy([Sprite('enemyR.txt', 100, 100, (255, 0, 0), 10, 6, screen), Sprite('enemyL.txt', 100, 100, (255, 0, 0), 10, 6, screen)], 2, 3, 3, 128, False, 100, [], 0, [], [])
+    faceTest = Enemy([Sprite('d.txt', 300, 200, (0, 255, 0), -1, 8, screen), Sprite('l.txt', 100, 100, (0, 255, 0), -1, 8, screen), Sprite('u.txt', 100, 100, (0, 255, 0), -1, 8, screen), Sprite('r.txt', 100, 100, (0, 255, 0), -1, 8, screen)], 4, 4, 1, 1, False, 1, [], 0, [], [[300, 200], [400, 200], [400, 300], [300, 300], 1])
     count = 0
     while running:
         for event in pygame.event.get():
@@ -26,6 +28,8 @@ def main():
         p1.takeInput(pygame.key.get_pressed())
         room1.update()
         p1.update(room1.returnAll())
+        testEnemy.update(p1, room1.returnAll())
+        faceTest.update(p1, room1.returnAll())
         if p1.getRoomChange() != 0:
             disp = Text('Touching', 0, 612, (255, 255, 255), 2, screen)
         else:
