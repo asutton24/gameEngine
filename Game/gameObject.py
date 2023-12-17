c651b31e349eb39a57ee10164b2b1fdef5157bb4
asutton24 <133972971+asutton24@users.noreply.github.com>
2023-12-17 15:40:32 -0500

    Add files via upload

--- Game/gameObject.py
+++ Game/gameObject.py
@@ -2,6 +2,14 @@ import pygame
 import random
 from sprite import *
 
+def sign(x):
+    if x == 0:
+        return 0
+    if x > 0:
+        return 1
+    return -1
+
+
 def rectCollide(x1, xlen1, y1, ylen1, x2, xlen2,  y2, ylen2, c):
     temp = (x1 <= x2 <= x1 + xlen1 or x1 <= x2 + xlen2 <= x1 + xlen1) and (y1 <= y2 <= y1 + ylen1 or y1 <= y2 + ylen2 <= y1 + ylen1)
     if c == 1:
@@ -10,9 +18,11 @@ def rectCollide(x1, xlen1, y1, ylen1, x2, xlen2,  y2, ylen2, c):
         return rectCollide(x2, xlen2, y2, ylen2, x1, xlen1, y1, ylen1, 1)
     else:
         return temp
+
+
 class GameObject:
 
-    def __init__(self, spr, sol, hel, gho):
+    def __init__(self, spr, sol, hel, gho, im):
         self.sprite = spr
         self.xVel = 0
         self.yVel = 0
@@ -21,14 +31,31 @@ class GameObject:
         self.health = hel
         self.alive = True
         self.ghost = gho
+        self.iFrames = 0
+        self.iMax = im
 
     def setHealth(self, x):
         self.health = x
         if self.health <= 0:
             self.alive = False
+        else:
+            self.alive = True
+
+    def damage(self, x):
+        if self.alive and self.iFrames == 0:
+            self.health -= x
+            self.iFrames = self.iMax
+            if self.health <= 0:
+                self.alive = False
 
     def getPos(self):
         return self.sprite.getPos()
+
+    def getCenter(self):
+        temp = self.sprite.getPos()
+        for i in range(2):
+            temp[i] += self.sprite.getPing() * self.sprite.getScale()/2.0
+        return temp
 
     def changeHealth(self, x):
         self.health += x
@@ -51,6 +78,7 @@ class GameObject:
 
     def kill(self):
         self.alive = False
+        self.health = 0
 
     def changeSprite(self, x):
         self.sprite = x
@@ -68,8 +96,15 @@ class GameObject:
                         return True
         return False
 
+    def quickUpdate(self):
+        if self.alive:
+            self.move(self.xVel, self.yVel)
+            self.sprite.update()
+
     def update(self, objs):
         if self.alive:
+            if self.iFrames > 0:
+                self.iFrames -= 1
             if self.knockback[2] != 0:
                 xMove = self.knockback[0]
                 yMove = self.knockback[1]
@@ -107,11 +142,15 @@ class Player(GameObject):
         self.moving = False
         self.newSprite = False
         self.roomChange = 0
-        GameObject.__init__(self, self.sprites[0], False, 100, False)
+        self.attacking = False
+        self.weapons = []
+        GameObject.__init__(self, self.sprites[0], False, 100, False, 30)
 
     def takeInput(self, keys):
         currentFacing = self.facing
         currentMove = self.moving
+        if keys[pygame.K_z]:
+            self.attack()
         if (keys[pygame.K_RIGHT] and keys[pygame.K_LEFT]) or (keys[pygame.K_UP] and keys[pygame.K_DOWN]):
             return
         horiMove = False
@@ -151,6 +190,16 @@ class Player(GameObject):
         self.roomChange = 0
         return temp
 
+    def giveWeapon(self, x):
+        self.weapons.append(x)
+
+    def attack(self):
+        x = self.getPos()[0]
+        y = self.getPos()[1]
+        for weapon in self.weapons:
+            weapon.shoot(x, y, self.facing)
+
+
     def update(self, objs):
         if self.newSprite:
             pos = self.sprite.getPos()
@@ -174,7 +223,7 @@ class Player(GameObject):
             self.sprite.updatePos(pos[0], pos[1])
             self.newSprite = False
         for i in objs:
-            if self.collideWith(self, i):
+            if type(i) == Tile and self.collideWith(self, i):
                 if i.getPurpose() == 'Up':
                     self.roomChange = 1
                 elif i.getPurpose() == 'Right':
@@ -183,13 +232,15 @@ class Player(GameObject):
                     self.roomChange = 3
                 elif i.getPurpose() == 'Left':
                     self.roomChange = 4
+        for i in self.weapons:
+            i.update(objs)
         GameObject.update(self, objs)
 
 class Tile(GameObject):
     def __init__(self, spr, s, b, h, p):
         self.breakable = b
         self.purpose = p
-        GameObject.__init__(self, spr, s, h, False)
+        GameObject.__init__(self, spr, s, h, False, 0)
 
     def getPurpose(self):
         return self.purpose
@@ -197,9 +248,11 @@ class Tile(GameObject):
     def breakTile(self):
         if self.breakable:
             self.alive = False
+            return True
+        return False
 
     def update(self):
-        GameObject.update(self, [])
+        self.quickUpdate()
 
     def toString(self):
         return r"Tile(Sprite('{}', {}, {}, {}, {}, {}, {}), {}, {}, {}, '{}')".format(self.sprite.path, self.sprite.x, self.sprite.y, self.sprite.color, self.sprite.animated, self.sprite.scale, 'self.screen', self.solid, self.breakable, self.health, self.purpose)
@@ -209,13 +262,16 @@ class Item(GameObject):
         self.purpose = p
         self.boost = b
         self.stackable = s
-        GameObject.__init__(self, spr, False, 1, False)
+        GameObject.__init__(self, spr, False, 1, False, 0)
 
     def getPurpose(self):
         return self.purpose
 
     def isBoost(self):
         return self.boost
+
+    def update(self):
+        self.quickUpdate()
 
     def toString(self):
         return r"Item(Sprite('{}', {}, {}, {}, {}, {}, {}), '{}', {}, {})".format(self.sprite.path, self.sprite.x,
@@ -229,7 +285,7 @@ class Item(GameObject):
         return self.stackable
 
 class Enemy(GameObject):
-    def __init__(self, spr, faceType, moveType, speed, moveLen, gho, health, drop, damage, proj, points):
+    def __init__(self, spr, faceType, moveType, speed, moveLen, gho, health, drop, damage, proj, points, im):
         if moveType == 4:
             self.points = points
             self.moving = False
@@ -237,21 +293,74 @@ class Enemy(GameObject):
         self.mType = moveType
         self.speed = speed
         self.drop = drop
-        self.damage = damage
+        self.hitDamage = damage
         self.fType = faceType
         self.currentDir = -1
         self.avgLen = moveLen
+        self.isAttacking = False
+        self.projectiles = []
+        self.projStatus = []
+        self.projectiles = []
+        self.tickClock = 0
+        if len(proj) == 4:
+            self.attackType = proj[0]
+            for i in range(proj[2]):
+                self.projectiles.append(proj[1])
+                self.projStatus.append(False)
+            self.chance = proj[3]
+        else:
+            self.attackType = None
+            self.chance = 0
+        if self.mType == 4:
+            im = 0
         if faceType == 1:
             self.sprites = 0
             self.facing = 0
-            GameObject.__init__(self, spr, False, health, gho)
+            GameObject.__init__(self, spr, False, health, gho, im)
         else:
             self.sprites = spr
-            GameObject.__init__(self, spr[0], False, health, gho)
+            GameObject.__init__(self, spr[0], False, health, gho, im)
             if faceType == 2:
                 self.facing = 1
             if faceType == 3 or faceType == 4:
                 self.facing = 2
+
+    def attack(self, player):
+        if not self.isAttacking and self.attackType is not None:
+            projIndex = 0
+            for i in self.projStatus:
+                if not i:
+                    self.projStatus[projIndex] = True
+                    break
+                projIndex += 1
+            if projIndex >= len(self.projStatus):
+                return
+            selfPos = self.getCenter()
+            playerPos = player.getCenter()
+            xLen = playerPos[0] - selfPos[0]
+            yLen = playerPos[1] - selfPos[1]
+            if self.attackType == 4:
+                xSign = sign(xLen)
+                ySign = sign(yLen)
+                xLen = abs(xLen)
+                yLen = abs(yLen)
+                normal = self.projectiles[projIndex].speed / ((xLen ** 2 + yLen ** 2) ** .5)
+                xLen *= (normal * xSign)
+                yLen *= (normal * ySign)
+                self.projectiles[projIndex].manualShoot(selfPos[0], selfPos[1], xLen, yLen)
+            elif self.attackType == 5:
+                if abs(xLen) > abs(yLen):
+                    if playerPos[0] > selfPos[0]:
+                        self.projectiles[projIndex].shoot(selfPos[0], selfPos[1], 1)
+                    else:
+                        self.projectiles[projIndex].shoot(selfPos[0], selfPos[1], 3)
+                else:
+                    if playerPos[1] > selfPos[1]:
+                        self.projectiles[projIndex].shoot(selfPos[0], selfPos[1], 2)
+                    else:
+                        self.projectiles[projIndex].shoot(selfPos[0], selfPos[1], 0)
+            else:
+                self.projectiles[projIndex].shoot(selfPos[0], selfPos[1], self.attackType)
 
     def changeFacing(self, x, y):
         # 0 is downward, increments clockwise
@@ -285,11 +394,13 @@ class Enemy(GameObject):
             self.updatePos(pos[0], pos[1])
             self.facing = x
 
-
     def update(self, player, objs):
         # 0: Stationary 1: Chase 2: Random move
         faceDir = []
+        if not self.alive:
+            return
         if self.mType == 0:
+            self.setKnockback(0, 0, 0)
             GameObject.update(self, [])
         elif self.mType == 1:
             self.setVel(0, 0)
@@ -403,39 +514,104 @@ class Enemy(GameObject):
                     self.points.append(1)
                     self.indexCount = 1
             GameObject.update(self, objs)
+        if self.chance < 1:
+            if random.random() <= self.chance:
+                self.attack(player)
+        else:
+            if self.tickClock >= self.chance:
+                self.attack(player)
+                self.tickClock = 0
+            self.tickClock += 1
+        counter = 0
+        foundFalse = False
+        for i in self.projStatus:
+            if i:
+                self.projectiles[counter].update(objs + [player])
+                self.projStatus[counter] = self.projectiles[counter].isShooting
+            if not i:
+                foundFalse = True
+            counter += 1
+        if foundFalse:
+            self.isAttacking = False
+        else:
+            self.isAttacking = True
+
 
 class Projectile(GameObject):
-    def __init__(self, spr, t, s, d, g, p):
+    def __init__(self, spr, t, s, d, g, p, pi, k, kf):
         self.lifetime = t
         self.ticks = 0
         self.speed = s
-        self.damage = d
+        self.hitDamage = d
         self.isShooting = False
         self.isPlayer = p
-        GameObject.__init__(self, spr, False, 1, g)
+        self.pierce = pi
+        self.knockVal = k
+        self.knockFrames = kf
+        self.knock = [0, 0, 0]
+        GameObject.__init__(self, spr, False, 1, g, 0)
+        self.kill()
+
+
+    def manualShoot(self, x, y, xs, ys):
+        if not self.isShooting:
+            self.isShooting = True
+            self.setHealth(1)
+            GameObject.updatePos(self, x, y)
+            self.ticks = 0
+            GameObject.setVel(self, xs, ys)
+            self.knock = [self.knockVal * sign(xs), self.knockVal * sign(ys)]
 
     def shoot(self, x, y, dir):
         if not self.isShooting:
             self.isShooting = True
+            self.setHealth(1)
             GameObject.updatePos(self, x, y)
             self.ticks = 0
             if dir == 0:
                 GameObject.setVel(self, 0, -self.speed)
+                self.knock = [0, -self.knockVal]
             elif dir == 1:
                 GameObject.setVel(self, self.speed, 0)
+                self.knock = [self.knockVal, 0]
             elif dir == 2:
                 GameObject.setVel(self, 0, self.speed)
+                self.knock = [0, self.knockVal]
             elif dir == 3:
                 GameObject.setVel(self, -self.speed, 0)
+                self.knock = [-self.knockVal, 0]
+
+    def status(self):
+        print('Am Shooting: {} Current tick: {} Alive: {}'.format(self.isShooting, self.ticks, self.alive))
 
     def update(self, objs):
         if self.isShooting:
             self.ticks += 1
             if self.ticks == self.lifetime:
                 self.isShooting = False
-            for i in objs:
-                if self.isPlayer and type(i) == Enemy:
-                    i = 0
+                GameObject.setVel(self, 0, 0)
+            if self.isShooting:
+                for i in objs:
+                    if self.isPlayer and type(i) == Enemy and self.collideWith(self, i):
+                        i.damage(self.hitDamage)
+                        i.setKnockback(self.knock[0], self.knock[1], self.knockFrames)
+                        if not self.pierce:
+                            self.kill()
+                            self.isShooting = False
+                    if not self.ghost and type(i) == Tile and self.collideWith(self, i):
+                        if self.isPlayer and i.breakTile():
+                            if not self.pierce:
+                                self.kill()
+                                self.isShooting = False
+                        else:
+                            self.kill()
+                            self.isShooting = False
+                    if not self.isPlayer and type(i) == Player and self.collideWith(self, i):
+                        i.damage(self.hitDamage)
+                        i.setKnockback(self.knock[0], self.knock[1], self.knockFrames)
+                        self.isShooting = False
+            self.quickUpdate()
+
 
 
 
