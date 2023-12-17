635e4ac392758fc14775c36545f6db696f85b2e6
asutton24 <133972971+asutton24@users.noreply.github.com>
2023-12-17 15:40:32 -0500

    Add files via upload

--- Game/runner.py
+++ Game/runner.py
@@ -6,18 +6,18 @@ from gameObject import *
 def main():
     running = True
     pygame.init()
-    screen = pygame.display.set_mode([1024, 720], pygame.FULLSCREEN)
+    screen = pygame.display.set_mode([1024, 720])
     clock = pygame.time.Clock()
     player = Sprite('player.txt', 500, 250, (255, 255, 255), 30, .5, screen)
     hello = Text('i can now/display/sprites', 0, 0, (255, 255, 255), 5, screen)
     colorSpr = ColorSprite('colorTest.txt', 100, 100, [(255, 0, 0), (0, 255, 0), (0, 0, 255)], -1, 5, screen)
     p1 = Player(500, 250, screen)
-    block = GameObject(Sprite('block.txt', 400, 200, (255, 255, 255), -1,5, screen), True, 1, False)
     room1 = Room('Manifest\\RoomManifest.txt', 0, (0, 0, 0), screen)
     room1.setDoors(True, True, True, True)
-    testEnemy = Enemy([Sprite('enemyR.txt', 100, 100, (255, 0, 0), 10, 6, screen), Sprite('enemyL.txt', 100, 100, (255, 0, 0), 10, 6, screen)], 2, 3, 3, 128, False, 100, [], 0, [], [])
-    faceTest = Enemy([Sprite('d.txt', 300, 200, (0, 255, 0), -1, 8, screen), Sprite('l.txt', 100, 100, (0, 255, 0), -1, 8, screen), Sprite('u.txt', 100, 100, (0, 255, 0), -1, 8, screen), Sprite('r.txt', 100, 100, (0, 255, 0), -1, 8, screen)], 4, 4, 1, 1, False, 1, [], 0, [], [[300, 200], [400, 200], [400, 300], [300, 300], 1])
+    testEnemy = Enemy([Sprite('enemyR.txt', 500, 300, (255, 0, 0), 10, 6, screen), Sprite('enemyL.txt', 100, 100, (255, 0, 0), 10, 6, screen)], 2, 0, 3, 128, False, 100, [], 0, [4, Projectile(Sprite('block.txt', 0, 0, (0, 255, 0), -1, 1, screen), 60, 5, 1, False, False, False, 5, 10), 1, .9999], [], 30)
+    #faceTest = Enemy([Sprite('d.txt', 300, 200, (0, 255, 0), -1, 8, screen), Sprite('l.txt', 100, 100, (0, 255, 0), -1, 8, screen), Sprite('u.txt', 100, 100, (0, 255, 0), -1, 8, screen), Sprite('r.txt', 100, 100, (0, 255, 0), -1, 8, screen)], 4, 4, 1, 1, False, 1, [], 0, [], [[300, 200], [400, 200], [400, 300], [300, 300], 1], 0)
     count = 0
+    p1.giveWeapon(Projectile(Sprite('block.txt', 0, 0, (255, 0, 0), -1, 1, screen), 60, 5, 1, False, True, False, 5, 10))
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -27,9 +27,9 @@ def main():
                     running = False
         p1.takeInput(pygame.key.get_pressed())
         room1.update()
-        p1.update(room1.returnAll())
+        p1.update(room1.returnAll() + [testEnemy])
         testEnemy.update(p1, room1.returnAll())
-        faceTest.update(p1, room1.returnAll())
+        #faceTest.update(p1, room1.returnAll())
         if p1.getRoomChange() != 0:
             disp = Text('Touching', 0, 612, (255, 255, 255), 2, screen)
         else:
