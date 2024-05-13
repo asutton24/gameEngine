226e1a567f8757fbf21c4a949b2274c511e8ac0b
asutton24 <133972971+asutton24@users.noreply.github.com>
2024-05-13 16:01:42 -0400

    Add files via upload

--- Game/bossTester.py
+++ Game/bossTester.py
@@ -6,7 +6,7 @@ running = True
 screen = pygame.display.set_mode([1024, 720], pygame.FULLSCREEN)
 p1 = Player(70, 270, screen)
 clock = pygame.time.Clock()
-room = BossRoom('bossRoom.room', (0, 0, 0), 0, [[3, 4], [5, 6]], 600, [1, 2], [1, 2], 300, 0, 240, screen)
+room = BossRoom('bossRoom.room', (0, 0, 0), 0, [[3, 4, 5, 6], [7, 8]], 600, [1, 2], [1, 2], 300, 0, 240, screen)
 p1.giveWeapon(Projectile(Sprite('block.spr', 0, 0, (255, 0, 0), -1, 1, screen), 12, 10, 10, False, True, False, 5, 10))
 while running:
     for event in pygame.event.get():
